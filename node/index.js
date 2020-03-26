23034fb62638789da676bfe1ecaa212f4bbdd17d
Everton Reis <evertonreis@Mac-mini-de-Everton.local>
2020-03-26 19:21:01 -0400

    criando projeto react

---
+++ node/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+  return response.json({
+    evento:'hello word',
+  });
+});
+app.listen(3333);
