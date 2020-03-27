193760f28c34c4065d1d5ce6ecbc9734e64b2d36
Everton Reis <evertonreis@Mac-mini-de-Everton.local>
2020-03-27 09:36:17 -0400

    metodo, rota  delete

--- node/src/routes.js
+++ node/src/routes.js
@@ -10,5 +10,8 @@ routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
 
 routes.get('/incidents', IncidentController.index);
+
 routes.post('/incidents', IncidentController.create);
+
+routes.delete('/incidents/:id', IncidentController.delete);
 module.exports = routes;
